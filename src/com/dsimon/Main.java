54be188c0b6edaf59c664c77b44e7f637cfd48a1
Hurlyburly2 <dougdougmann@gmail.com>
2019-06-17 01:53:51 -0400

    lesson complete

--- src/com/dsimon/Main.java
+++ src/com/dsimon/Main.java
@@ -3,6 +3,13 @@ package com.dsimon;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        // a mile is equal to 1.609344 km
+        double kilometers = (100 * 1.609344);
+
+        int highScore = 50;
+        if (highScore == 50) {
+            System.out.println("This is an expression.");
+        }
     }
 }
